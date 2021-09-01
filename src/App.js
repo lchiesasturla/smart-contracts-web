eaedbb26df734dfabc86574e37b916b0b1e73b1e
lchiesasturla <lchiesasturla@frba.utn.edu.ar>
2021-09-01 15:07:01 -0300

    :construction: Work in progress on contract creation :construction:

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ContractList from './pages/contracts/ContractList';
-import NavigationBar from "./components/layout/NavigationBar";
 import Topbar from "./components/layout/Topbar";
-import ContractCreation from "./pages/contracts/ContractCreation/ContactCreation";
+import ContractCreation from "./pages/contracts/ContractCreation";
 
 function App() {
   return (
     <Router>
       <Topbar/>
-      <NavigationBar/>
       <Switch>
         <Route exact path="/">
           <ContractList />
--- src/pages/contracts/ContractCreation/index.js
+++ src/pages/contracts/ContractCreation/index.js
@@ -1,1 +1,1 @@
-export { default } from './ContactCreation';
+export { default } from './ContractCreation';
